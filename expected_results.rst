7257e6e09dcb12afb7ecc111c4ba5c0e3e035980
hamogu <moritz.guenther@gmx.de>
2013-03-29 17:43:14 -0400

    Generate lightcurves for testing in a script

---
+++ expected_results.rst
@@ -0,0 +1,119 @@
+Testing our YSOVAR codes
+========================
+We have generated artifical csv files that have the same format (at at
+least a very similar one) to the csv files extracted from the YSOVAR2
+database.
+The purpose of this excercise is
+- test that our code works correctly
+- compare our python code to Maria's IDL code
+
+Just like the real data, one singe csv file contains many different
+sources and each source is meant to verify one or more of the
+statistics we use. Most of them are analytically generated, so we know
+what the result should be exactly, some are randomly generated. They
+might change between different run (I'll implement a constant seed of
+the random number generator later).
+
+Parameters that are uninteresting for the test in question (e.g. RA,
+DEC or the magnitudes when we only care for the number of datapoints)
+are randomly generated and will also change each time we rerun the
+program. Also, I did not bother to set the number of digits for each
+item, so that e.g. `ra` and `de` might have less digits and `mag1` might
+have more than in the csv files. Let me know if that is a problem, I can fix that.
+
+All sources in the csv file have negative `ysovarid` entries to set
+them apart form real data. Below, we list for each source which
+statistics we want to verify there.
+
+.. Note:: All the examples below assume that no systematic error (i.e.
+   no error-floor is added). If you *do* add an error floor, then
+   obviously all values that depend on the error (chi^2, stetson)
+   will not come out as analytically calculated.
+
+
+
+
+
+One-color statistics
+--------------------
+
+constant lightcurves
+^^^^^^^^^^^^^^^^^^^^
+(5 entries)
+- number of entries = 5 in the band chosen
+- max = 12
+- min = 12 
+- median = 12
+- weighted mean = 12
+- any quantile = 12
+- standard deviation = 0 
+- median absolute devitation = 0
+- chi^2 to mean = 0
+
+Source number band  lightcurve  
+============= ===== ===========
+-1000         IRAC1 constant 
+-1001         IRAC2 constant
+
+simple lightcurves
+^^^^^^^^^^^^^^^^^^
+(5 entries)
+- number of entries = 5 in the band chosen
+- max = 12
+- min = 12 
+- median = 12
+- weighted mean = 12
+- standard deviation = 1/sqrt(2) 
+- median absolute devitation = 0.5
+- chi^2 to mean = 15.81
+
+Source number band  lightcurve  
+============= ===== ==================
+-1002         IRAC1 linear brightening
+-1003         IRAC2 linear brightening
+
+Periodicity
+-----------
+All lightcurves here are generated with a sin-wave, most have some
+added noise, so the expected results can vary a little. We add the noise,
+because some implementations of Lomb-Scargle fail, if the solution is
+exact (the peak of the periodogram could be infinite).
+
+Source number band  Noise? expected results
+============= ===== ====== ================================
+-1500         IRAC1 no     period = 3
+-1501         IRAC1 yes    period = 50, peak is significant
+-1502         IRAC2 yes    no significant peak
+-1503         both  yes    period = 3 in IRAC1, =5 in IRAC2
+                           IRAC1 is more significant
+
+
+Merging lightcurves with data in different bands
+------------------------------------------------
+The following lightcurves have different observation times in IRAC1
+and IRAC2 (e.g. IRAC1 before IRAC2, IRAC1 after IRAC2). The table
+gives the expected number of datapoints where photometry for IRAC1 and
+IRAC2 exisits within 0.01 days or 0.05 days.
+
+Source number 0.01 days 0.05 days
+============= ========= =========
+-2000         none      none
+-2001         50        50
+-2002         10        50
+
+Two-color statistics
+--------------------
+Here are a few sources to check shapes in the color-magnitude-diagram.
+
+Source number color change CMD shape
+============= ============ ===========
+-2500         None         flat   
+-2501         random       point cloud
+-2502         reddening    line
+
+And now the Stetson index
+
+Source number Stetson index
+============= =============
+-2700         0.0000
+-2701         1.0102
---
+++ test_lcs.py
@@ -0,0 +1,116 @@
+import sys
+import numpy as np
+from numpy.random import random
+import astropy.table as table
+
+
+csvcols = ["ysovarid","sname","ra","de","hmjd","fname","mag1","emag1","useful","kind","note"]
+coltype = [np.int, 'S23', np.float, np.float, np.float, 'S5', np.float, np.float, np.int, np.int, 'S20']
+
+def add_source(table, time, mag, mag_error, ysovarid, fname, sname = None, ra = None, dec = None, useful = 1, kind = 0, note = ''):
+    '''Add info about a fake source to the table.
+
+    If ra or dec ad None, then look if a source with same ID exisits in the
+    table already. If so, copy their value, if not, generate randomnly.
+    '''
+    if len(time) != len(mag):
+        raise ValueError('Time and mag need to have same number of elements')
+    if len(mag) != len(mag_error):
+        raise ValueError('mag and mag_error need to have some number of elements')
+    if ra is None:
+        ind = np.nonzero(tab['ysovarid'] == ysovarid)[0]
+        if len(ind) > 0:
+            ra = table['ra'][ind][0]
+        else:
+            ra = np.random.random() * 360.
+    if dec is None:
+        ind = np.nonzero(tab['ysovarid'] == ysovarid)[0]
+        if len(ind) > 0:
+            dec = table['de'][ind][0]
+        else:
+           dec = (np.random.random() * 180) - 90.
+    sname = sname or 'test_'+str(abs(ysovarid))
+    for i in range(len(time)):
+        table.add_row({"ysovarid": ysovarid,"sname": sname,"ra":ra,"de":dec,"hmjd":time[i],"fname": fname,"mag1":mag[i],"emag1":mag_error[i],"useful":useful,"kind":kind,"note":note})
+
+
+tab = table.Table()
+for name, dtype in zip (csvcols, coltype):
+    col = table.Column(name = name, length=0, dtype = dtype)
+    tab.add_column(col)
+
+# create a few dummy times, that look OK that can be reused, when time
+# does not matter
+t5 = 55300.5 + np.arange(5)
+t50 = 55500.2 + np.arange(50)
+
+
+### check min, max, etc.
+### source numbers begin with -1000
+add_source(tab, t5, [12.,12.,12.,12.,12.], [.1,.1,.1,.1,.1], -1000, "IRAC1")
+add_source(tab, t5, [12.,12.,12.,12.,12.], [.1,.1,.1,.1,.1], -1001, "IRAC2")
+
+add_source(tab, t5, [13.,12.5,12.,11.5,11.], [.1,.1,.1,.1,.1], -1002, "IRAC1")
+add_source(tab, t5, [13.,12.5,12.,11.5,11.], [.1,.1,.1,.1,.1], -1003, "IRAC2")
+
+### testing periodicity finding
+t100 = 55500. + np.arange(100) + random(100)
+add_source(tab, t100, np.sin(t100*2*np.pi/3), 0.01+np.zeros_like(t100), -1500, "IRAC1")
+
+add_source(tab, t100, np.sin(t100/50*2*np.pi) + random(100) * 0.01, 0.01+np.zeros_like(t100), -1501, "IRAC1")
+
+add_source(tab, t100, 12. + np.sin(t100/5*2*np.pi)/20. + 2.*random(100), 0.5+random(100), -1502, "IRAC2")
+
+add_source(tab, t100, np.sin(t100/3*2*np.pi) + random(100) * 0.01, 0.01+random(100), -1503, "IRAC1")
+add_source(tab, t100, np.sin(t100/5*2*np.pi) + random(100)*10., 10.+np.zeros_like(t100), -1503, "IRAC2")
+
+
+
+
+### testing multiband lc merging
+add_source(tab, t50, 12.+ random(50), random(50), -2000, 'IRAC1')
+t_2000 = t50
+t_2000[:25] = t50[:25] - 0.06 - random(25)/0.5
+t_2000[25:] = t50[25:] + 0.06 + random(25)/0.5
+add_source(tab, t_2000, 12. + random(50), random(50), -2000, 'IRAC2')
+
+add_source(tab, t50, 12.+ random(50), random(50), -2001, 'IRAC1')
+add_source(tab, t50 + random(50)/0.01, 12.+ random(50), random(50), -2001, 'IRAC2')
+
+add_source(tab, t50, 12.+ random(50), random(50), -2002, 'IRAC1')
+add_source(tab, t50 + np.linspace(-0.049, +0.049), 12.+ random(50), random(50), -2002, 'IRAC2')
+
+### testing CMDs
+add_source(tab, t50, 12. + np.linspace(-1,1), 0.1+np.zeros(50), -2500, 'IRAC1')
+add_source(tab, t50, 11. + np.linspace(1,-1), 0.1+np.zeros(50), -2500, 'IRAC2')
+
+add_source(tab, t50, 12. + random(50), 0.1+np.zeros(50), -2501, 'IRAC1')
+add_source(tab, t50, 11. + random(50), 0.1+np.zeros(50), -2501, 'IRAC2')
+
+# relative absorption coeff from Flaherty et al. (2007) ApJ, 663, 1069
+add_source(tab, t50, 12. + np.linspace(0, 3) * 0.632, 0.1+np.zeros(50), -2502, 'IRAC1')
+add_source(tab, t50, 11. + np.linspace(0, 3) * 0.53 , 0.1+np.zeros(50), -2502, 'IRAC2')
+
+add_source(tab, t50, 12. + np.linspace(-1,1), 0.1+np.zeros(50), -2500, 'IRAC1')
+add_source(tab, t50, 11. + np.linspace(1,-1), 0.1+np.zeros(50), -2500, 'IRAC2')
+
+
+### testing Stetson
+add_source(tab, t50, 12. + np.linspace(-1,1), 0.1+np.zeros(50), -2700, 'IRAC1')
+add_source(tab, t50, 11. + np.linspace(1,-1), 0.1+np.zeros(50), -2700, 'IRAC2')
+
+mags = np.ones(50)
+mags[25:] = -1
+add_source(tab, t50, 12. + mags, 0.1 + np.zeros(50), -2701, 'IRAC1')
+add_source(tab, t50, 13. + mags, 0.1 + np.zeros(50), -2701, 'IRAC2')
+
+
+#### output the stuff
+
+import csv
+
+with open(sys.argv[1], 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
+    writer.writerow(tab.colnames)
+    for i in range(len(tab)):
+        writer.writerow(tab[i])
